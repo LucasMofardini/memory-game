7c77579b446fd6eba1bd97617ab413bd3752ee47
Lucas Mofardini <lucasmofardini@gmail.com>
2023-07-09 22:59:30 -0300

    Showing cards, toggle cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Card = ({ children }) => {
+const Card = ({ isActive = true, children, ...props }) => {
     return(
-        <div className='w-36 h-36 bg-slate-400 rounded-md flex justify-center items-center'>
-            <p className='font-mono text-3xl text-gray-600'>{children}</p>
+        <div className='w-36 h-36 bg-slate-400 rounded-md flex justify-center items-center' {...props} >
+            <p className='font-mono text-3xl text-gray-600'>{ isActive ? children : null }</p>
         </div>
     )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
 
-const arr =  Array(16).fill(0);
+const arr = [
+    { active: false, value: '1', id: 1 },
+    { active: false, value: '1', id: 2 },
+    { active: false, value: '2', id: 3 },
+    { active: false, value: '2', id: 4 },
+    { active: false, value: '3', id: 5 },
+    { active: false, value: '3', id: 6 },
+    { active: false, value: '4', id: 7 },
+    { active: false, value: '5', id: 8 }
+];
 
-const createCards = (qtd) => {
-    
-}
+const Cards = () => {
 
-const Cards = ({ objcts }) => {
+    const [cards, setCards] = useState(null);
+
+    const createCards = (pairsQtd = 8) => {
+        setCards(arr);
+    }
+
+    const toggleCard = (id) => {
+        const filtered = cards.map((card) => {
+            let item = card;
+            if(card.id === id)
+                item.active = !item.active
+            return item;
+        })
+
+        setCards(filtered)
+    }
+
+    useEffect(() => createCards, [])
+
     return(
         <div className='flex justify-center items-center w-full h-screen bg-emerald-100'>
             <div className='w-2/5'>
                 <div className="grid grid-flow-col grid-rows-4 grid-cols-4 gap-4 drop-shadow-md hover:cursor-pointer">
-                    {arr.map((c, i) => <Card key={c + i}> {c} </Card> )} 
+                    {cards && cards.map((c, i) => <Card onClick={() => toggleCard(c.id)} id={c.id} key={c + i} isActive={c.active}> {c.value} </Card> )} 
                 </div>
             </div>
         </div>
